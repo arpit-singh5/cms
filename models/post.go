523256f5bf9b962a0e33faeb2dda1220c133c594
Arpit <arpitsinghraput0909@gmail.com>
2026-01-08 10:43:01 +0530

    Post Status Logic Updated

--- models/post.go
+++ models/post.go
@@ -2,19 +2,40 @@ package models
 
 import "time"
 
+type PostStatus string
+
+const (
+	StatusDraft     PostStatus = "draft"
+	StatusPublished PostStatus = "published"
+	StatusArchived  PostStatus = "archived"
+)
+
 type Post struct {
-	ID uint
-	UserId uint
-	Title string
-	Slug string
-	Description string
-	Content string
+	ID            uint
+	UserId        uint
+	Title         string
+	Slug          string
+	Description   string
+	Content       string
 	FeaturedImage string
-	CreatedAt time.Time
+	Status        PostStatus
+	CreatedAt     time.Time
+	PublishedAt   time.Time
+	UpdatedAt     time.Time
+}
+
+func (s PostStatus) IsValid() bool {
+	switch s {
+	case StatusArchived, StatusDraft, StatusPublished:
+		return true
+	default:
+		return false
+
+	}
 }
 
 type Tag struct {
-	ID uint
+	ID   uint
 	Name string
 	Slug string
 }
